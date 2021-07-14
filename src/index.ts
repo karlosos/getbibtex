1ee9d0a3b726221ddf0193a48bb9f3a3823f951c
Karol Dzialowski <karlososhd@gmail.com>
2021-07-14 12:21:38 +0200

    Update citationkey

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import urlMetadata from 'url-metadata'
 import {parseDomain, ParseResultType} from "parse-domain";
 
-const url: string = 'https://www.dzialowski.eu/great-depression-roth/'
-// const url = 'https://www.theguardian.com/us-news/2021/jul/13/joe-biden-republicans-voting-rights-philadelphia'
+// const url: string = 'https://www.dzialowski.eu/great-depression-roth/'
+const url = 'https://www.theguardian.com/us-news/2021/jul/13/joe-biden-republicans-voting-rights-philadelphia'
 
 interface EntryData {
     title: string,
@@ -22,26 +22,22 @@ function firstWordFromTitle(title: string): string {
 }
 
 function createCiteKey(entryData: EntryData): string {
-    // TODO: check if there are no invalid characters for bibtex
     let citekey: string = ""
-    if (entryData.author !== "") {
-        citekey = entryData.author + firstWordFromTitle(entryData.title)
-    } else {
-        const parseResult = parseDomain(entryData.website)
-        if (parseResult.type === ParseResultType.Listed) {
-            const {subDomains, domain, topLevelDomains} = parseResult
-            citekey = "" + domain
-        }
-        citekey = citekey + firstWordFromTitle(entryData.title)
+    const parseResult = parseDomain(entryData.website)
+    if (parseResult.type === ParseResultType.Listed) {
+        const {subDomains, domain, topLevelDomains} = parseResult
+        citekey = "" + domain
     }
+    citekey = citekey + firstWordFromTitle(entryData.title)
+    citekey = citekey.replace(/\W/g, '') // remove non-alphanumeric characters
     return citekey
 }
 
 function bibtexFromEntryData(entryData: EntryData): string {
     // TODO: check if there are no invalid characters for bibtex
-    let bibtex: string = `@misc{${createCiteKey(entryData)}
+    let bibtex: string = `@misc{${createCiteKey(entryData)},
 \tauthor = "${entryData.author}",
-\ttitle = "${entryData.title} -- ${entryData.website}",
+\ttitle = "{${entryData.title}} --- ${entryData.website}",
 \thowpublished = "\\url{${entryData.url}}",
 \tyear = {},
 \tnote = {(Accessed on 14.07.2021)},

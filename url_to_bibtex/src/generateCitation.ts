77e24fc5b0d19730ea6e93d86f5db6dc3d91a4a3
Karol Dzialowski <karlososhd@gmail.com>
2021-07-19 18:07:08 +0200

    Upper letters enclosed in curly brackets

--- url_to_bibtex/src/generateCitation.ts
+++ url_to_bibtex/src/generateCitation.ts
@@ -36,10 +36,11 @@ function createCiteKey(entryData: EntryData): string {
 
 function bibtexFromEntryData(entryData: EntryData): string {
     // TODO: check if there are no invalid characters for bibtex
-    const currentDate = moment().format("DD-MMM-YYYY"); 
+    const currentDate = moment().format("DD-MMM-YYYY")
+    const title = upperLettersInBibTex(`${entryData.title}} --- ${entryData.website}`)
     let bibtex: string = `@misc{${createCiteKey(entryData)},
-\tauthor = "${entryData.author}",
-\ttitle = "{${entryData.title}} --- ${entryData.website}",
+\tauthor = "{${entryData.author}}",
+\ttitle = "{${title}}",
 \thowpublished = "\\url{${entryData.url}}",
 \tyear = {},
 \tnote = {[Accessed ${currentDate}]},
@@ -47,9 +48,13 @@ function bibtexFromEntryData(entryData: EntryData): string {
     return bibtex
 }
 
+function upperLettersInBibTex(str: string): string {
+    return str.replace(/([A-Z])/g, '{$1}')
+}
+
 function domainFromUrl(url: string): string {
     // based on regular expression https://regex101.com/r/MOIFTy/3
-    const domainRegex = '^(?:https?:)?(?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)'
+    const domainRegex = /^(?:https?:)?(?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n]+)/
     const match = url.match(domainRegex)
     let domain: string = ''
     if (match != null) {

138d005c754d3e447aec7275925559311d094393
Karol Dzialowski <karlososhd@gmail.com>
2021-07-21 12:52:24 +0200

    Add test, upadte README, fix code with standardjs

--- README.md
+++ README.md
@@ -1,12 +1,41 @@
-# URL to BibTex
+![vscode-portfolio banner](./docs/animation.gif)
 
-Get BibTex entry for a website. Data is scrapped from website headers.
+<div align="center">
 
-## Why proxy server (cors-anywhere)?
+[![Code style: standardjs](https://img.shields.io/badge/code%20style-standardjs-F3DF49.svg)](https://standardjs.com/)
+[![Build and Deploy](https://github.com/karlosos/url_to_bibtex/actions/workflows/main.yml/badge.svg)](https://github.com/karlosos/url_to_bibtex/actions/workflows/main.yml)
+</div>
 
-Frontend application cannot make requests to other domains, because of [same-origin policy](https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy). That's why there's need of proxy server that adds CORDS headers to the proxied request. You can read more about [Cross-Origin Resource Sharing here](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS).
+***
 
-I've used [cors-anywhere](https://github.com/Rob--W/cors-anywhere), because of it's simplicity and easy of deployment to Heroku.
+<h4 align="center">BibTex entry generator from URL</h4>
+
+
+<p align="center">
+  <a href="#about">About</a> •
+  <a href="#what-i-have-learned">What I Have Learned</a> •
+  <a href="#development">Development</a> •
+  <a href="#deployment">Deployment</a>
+</p>
+
+<p align="center">
+<table>
+<tbody>
+<td align="center">
+<img width="2000" height="0"><br>
+Website: <b><a href="https://karlosos.github.io/url_to_bibtex/">karlosos.github.io/url_to_bibtex 🌐</a></b><br>
+</td>
+</tbody>
+</table>
+</p>
+
+## About
+
+Get BibTex entry for a website. Data is scrapped from website headers. The project was inspired by [Wikipedia BibTeX Generator](https://irl.github.io/bibwiki/) which works only for Wikipedia links.
+
+## What I Have Learned
+
+This project was more goal oriented rather than education oriented. I've mostly polished my knowledge in CORS/same origin policy areas.
 
 ## Development
 
@@ -17,7 +46,14 @@ I've used [cors-anywhere](https://github.com/Rob--W/cors-anywhere), because of i
 1. Run cors-anywhere server with `node server.js`.
 1. Access application under `localhost:3000`.
 
-## Deploy
+### Why proxy server (cors-anywhere)?
+
+Frontend application cannot make requests to other domains, because of [same-origin policy](https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy). That's why there's need of proxy server that adds CORDS headers to the proxied request. You can read more about [Cross-Origin Resource Sharing here](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS).
+
+I've used [cors-anywhere](https://github.com/Rob--W/cors-anywhere), because of it's simplicity and easy of deployment to Heroku.
+
+
+## Deployment
 
 Add environmental variable (project secrets on Github):
 
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
-import { GeistProvider, CssBaseline, useClipboard } from '@geist-ui/react'
-import { Row, Col, Input, Spacer, Button, Textarea, Text, Note } from '@geist-ui/react'
+import { GeistProvider, CssBaseline, useClipboard, Row, Col, Input, Spacer, Button, Textarea, Text, Note } from '@geist-ui/react'
+
 import { useEffect, useState } from 'react'
 import { getCitation } from './generateCitation'
 import { Copy, Check } from '@geist-ui/react-icons'
@@ -20,10 +20,10 @@ const App = () => {
 
   const handleButtonClicked = (e) => {
     setLoading(true)
-    getCitation(url, setBibtexEntry).then(({bibtex, entryData})=> {
+    getCitation(url, setBibtexEntry).then(({ bibtex, entryData }) => {
       setBibtexEntry(bibtex)
       setEntryData(entryData)
-      setLoading(false)  // will this work on error promise?
+      setLoading(false) // will this work on error promise?
     })
   }
 
@@ -43,34 +43,33 @@ const App = () => {
   return (
     <GeistProvider>
       <CssBaseline />
-        <Row style={{ marginBottom: '15px'}} justify="center">
-          <Col span={22} justify="center" style={{maxWidth: '38em'}}>
-            <Spacer y={2}/>
-            <Text h1>BibTex generator from URL</Text>
-            <Note label="IMPORTANT NOTE" type="warning">Most educators and professionals do not consider it appropriate to use tertiary sources such as encyclopedias as a sole source for any information—citing an encyclopedia as an important reference in footnotes or bibliographies may result in censure or a failing grade. Wikipedia articles should be used for background information, as a reference for correct terminology and search terms, and as a starting point for further research.</Note>
-            <Spacer y={2} />
-            <Text h4>Enter the URL below: </Text>
-            <Input placeholder="URL to website/article" width="100%" value={url} onChange={handleUrlChange} />
-            <Spacer y={.5} />
-            <Row justify="center">
-              <Button loading={loading} onClick={handleButtonClicked} type="secondary" style={{width: '100%'}}>Generate BibTex entry</Button>
-            </Row>
-            <Spacer y={.5} />
-            <div className='output' style={{position: 'relative'}}>
-            <Textarea placeholder="" width="100%" minHeight="23em" value={bibtexEntry} onChange={(e) => setBibtexEntry(e.target.value)}/>
-            <Button style={{position: 'absolute', top: '5px', right: '5px', zIndex: 33, borderColor: copied ? 'green' : '', padding: '0 0.5rem'}} iconRight={copied ? <Check color='green' /> : <Copy /> } auto size="small" onClick={handleCopyClicked}></Button>
-            </div>
-            {entryData!=null && 
-              <span>
-                <span>{entryData?.author && entryData?.author + '. '}</span>
-                <span style={{fontStyle: 'italic'}}>{entryData?.title + ' --- ' + entryData?.website + '. '} </span>
-                <a href={entryData?.url}>{entryData?.url}</a>,
-                <span> {entryData?.date && entryData?.date + '. '}</span>
-                <span>[Accessed {moment().format("DD-MMM-YYYY")}]</span>
-              </span>
-            }
-          </Col>
-        </Row>
+      <Row style={{ marginBottom: '15px' }} justify='center'>
+        <Col span={22} justify='center' style={{ maxWidth: '38em' }}>
+          <Spacer y={2} />
+          <Text h1>BibTex generator from URL</Text>
+          <Note label='IMPORTANT NOTE' type='warning'>Most educators and professionals do not consider it appropriate to use tertiary sources such as encyclopedias as a sole source for any information—citing an encyclopedia as an important reference in footnotes or bibliographies may result in censure or a failing grade. Wikipedia articles should be used for background information, as a reference for correct terminology and search terms, and as a starting point for further research.</Note>
+          <Spacer y={2} />
+          <Text h4>Enter the URL below: </Text>
+          <Input placeholder='URL to website/article' width='100%' value={url} onChange={handleUrlChange} />
+          <Spacer y={0.5} />
+          <Row justify='center'>
+            <Button loading={loading} onClick={handleButtonClicked} type='secondary' style={{ width: '100%' }}>Generate BibTex entry</Button>
+          </Row>
+          <Spacer y={0.5} />
+          <div className='output' style={{ position: 'relative' }}>
+            <Textarea placeholder='' width='100%' minHeight='23em' value={bibtexEntry} onChange={(e) => setBibtexEntry(e.target.value)} />
+            <Button style={{ position: 'absolute', top: '5px', right: '5px', zIndex: 33, borderColor: copied ? 'green' : '', padding: '0 0.5rem' }} iconRight={copied ? <Check color='green' /> : <Copy />} auto size='small' onClick={handleCopyClicked} />
+          </div>
+          {entryData != null &&
+            <span>
+              <span>{entryData?.author && entryData?.author + '. '}</span>
+              <span style={{ fontStyle: 'italic' }}>{entryData?.title + ' --- ' + entryData?.website + '. '} </span>
+              <a href={entryData?.url}>{entryData?.url}</a>,
+              <span> {entryData?.date && entryData?.date + '. '}</span>
+              <span>[Accessed {moment().format('DD-MMM-YYYY')}]</span>
+            </span>}
+        </Col>
+      </Row>
     </GeistProvider>
   )
 }

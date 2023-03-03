e6a3b6b4dbed04b25332015dc3e771e988cc4183
Karol Dzialowski <karlososhd@gmail.com>
2023-03-03 20:36:56 +0100

    SSR with @geist-ui take 2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,25 @@
-import { Fragment } from "react";
 import Document, {
   Html,
   Head,
   Main,
   NextScript,
   DocumentContext,
-  DocumentInitialProps,
 } from "next/document";
 import { CssBaseline } from "@geist-ui/core";
 
 class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     const styles = CssBaseline.flush();
 
     return {
       ...initialProps,
-      styles: [
-        <Fragment key="1">
+      styles: (
+        <>
           {initialProps.styles}
           {styles}
-        </Fragment>,
-      ],
+        </>
+      ),
     };
   }
 
@@ -40,4 +36,4 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument
+export default MyDocument;

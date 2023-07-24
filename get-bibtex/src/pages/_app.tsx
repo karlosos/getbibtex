2ebf040b96d83fc9fa383fda81bb7bfd28833e9c
Karol Dzialowski <karlososhd@gmail.com>
2023-07-24 18:32:25 +0200

    add google analytics to t3

--- get-bibtex/src/pages/_app.tsx
+++ get-bibtex/src/pages/_app.tsx
@@ -3,23 +3,39 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "@/utils/api";
 import "@/styles/globals.css";
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <main className={`${inter.variable} font-sans`} >
-        <Component {...pageProps} />
-      </main>
-    </SessionProvider>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-J3JZ08YKE2"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-J3JZ08YKE2');
+        `}
+      </Script>
+      <SessionProvider session={session}>
+        <main className={`${inter.variable} font-sans`}>
+          <Component {...pageProps} />
+        </main>
+      </SessionProvider>
+    </>
   );
 };
 

bbf8c6742481d9fdbf9d517b1ae37597356fb9d3
Karol Dzialowski <karlososhd@gmail.com>
2023-10-13 21:19:25 +0200

    errors on relative axis

--- src/components/admin/overview.tsx
+++ src/components/admin/overview.tsx
@@ -14,17 +14,21 @@ export function UsageChart({ className }: { className?: string }) {
   const { data: urlsCountPerDays } = api.stats.getUrlsCountPerDays.useQuery();
   const { data: errorsCountPerDays } =
     api.stats.getErrorsCountPerDays.useQuery();
-  
+
   const data =
     urlsCountPerDays
       ?.map((urlsData, i) => ({
         label: formatDayMonth(urlsData.date),
         date: urlsData.date,
         urlsCount: urlsData.count,
-        errorsCount: errorsCountPerDays ? errorsCountPerDays[i]?.count : 0, // TODO: find a better way to combine those two arrays
+        errorsCount: errorsCountPerDays
+          ? calculateErrorsPercentage(
+              urlsData.count,
+              errorsCountPerDays[i]?.count,
+            )
+          : 0, // TODO: find a better way to combine those two arrays
       }))
       .reverse() ?? [];
-
 
   return (
     <Card className={className}>
@@ -42,21 +46,54 @@ export function UsageChart({ className }: { className?: string }) {
               axisLine={false}
             />
             <YAxis
+              yAxisId="left"
               stroke="#888888"
               fontSize={12}
               tickLine={false}
               axisLine={false}
               tickFormatter={(value: any) => `${value}`}
             />
+            <YAxis
+              yAxisId="right"
+              orientation="right"
+              stroke="#888888"
+              fontSize={12}
+              tickLine={false}
+              axisLine={false}
+              tickFormatter={(value: any) => `${value}`}
+              domain={[0, 100]}
+            />
             <Tooltip
               cursor={{ fill: "#cfe3df" }}
               contentStyle={{ borderRadius: "8px" }}
             />
-            <Bar dataKey="urlsCount" fill="#50ad99" radius={[4, 4, 0, 0]} />
-            <Bar dataKey="errorsCount" fill="#ffaac2" radius={[4, 4, 0, 0]} />
+            <Bar
+              yAxisId="left"
+              dataKey="urlsCount"
+              fill="#50ad99"
+              radius={[4, 4, 0, 0]}
+            />
+            <Bar
+              yAxisId="right"
+              dataKey="errorsCount"
+              fill="#ffaac2"
+              radius={[4, 4, 0, 0]}
+            />
           </ComposedChart>
         </ResponsiveContainer>
       </CardContent>
     </Card>
   );
 }
+
+const calculateErrorsPercentage = (urlsCount: number, errorsCount?: number) => {
+  // Calculates errors percentage in relative to urlsCount
+  // Returns number with 2 digits
+  let res = 0;
+
+  if (errorsCount) {
+    res = (errorsCount / urlsCount) * 100;
+  }
+
+  return Number(res.toFixed(2));
+};

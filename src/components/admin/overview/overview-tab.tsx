62dec1b2de73cdfb492371ef3f4241cdd93c040c
Karol Dzialowski <karlososhd@gmail.com>
2024-02-11 15:06:17 +0100

    upgrade packages

--- src/components/admin/overview/overview-tab.tsx
+++ src/components/admin/overview/overview-tab.tsx
@@ -161,9 +161,9 @@ export const OverviewTab = () => {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               className="h-4 w-4 text-muted-foreground"
             >
               <rect width="18" height="11" x="3" y="11" rx="2" ry="2"></rect>
--- src/pages/auth/unauthorized.tsx
+++ src/pages/auth/unauthorized.tsx
@@ -23,13 +23,13 @@ export default function Unauthorized() {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke-width="1.5"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     className="h-5 w-5 rtl:rotate-180"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
                     />
                   </svg>

1ed922b5d674556d64aa52de8360773bb0249557
Radiirgummii <104864594+Radiirgummii@users.noreply.github.com>
2024-05-02 20:37:01 +0200

    fixed _ in title

--- src/utils/bibtex-encode-characters.ts
+++ src/utils/bibtex-encode-characters.ts
@@ -2,5 +2,6 @@ export function encodeCharactersInBibTex(str: string): string {
   return str
     .replace(/([A-Z])/g, "{$1}")
     .replace(/&/g, "\\&")
-    .replace(/\$/g, "\\$");
+    .replace(/\$/g, "\\$")
+    .replace(/\_/g, "\\_");
 }
